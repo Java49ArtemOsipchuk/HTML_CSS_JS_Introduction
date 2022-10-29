ac4b3106d6596745d1fa7495a5743789671be342
ArtemOsipchuk <artyom.osipchuk92@gmail.com>
2022-10-29 10:19:26 +0300

    HW #10

--- src/index.js
+++ src/index.js
@@ -4,16 +4,26 @@ const anchors = document.querySelectorAll(".thumbnails-anchor");
 const detailsImage = document.querySelector(".details-image");
 const detailsTitle = document.querySelector(".details-title");
 const mainClass = document.querySelector(".main-class");
+const detailsContainer = document.querySelector(".details-container");
+const audio = document.getElementById("audio");
+
 const HIDDEN = "hidden";
+const IS_POINT = "is-point";
 
 function setDetails (anchor) {
     detailsImage.setAttribute("src", anchor.getAttribute("data-details-image"));
+    audio.setAttribute("src", anchor.getAttribute("data-audio"));
     detailsTitle.innerHTML = anchor.getAttribute("data-details-title");
+    audio.play();
+    setTimeout(function() {
+        audio.pause();
+        audio.currentTime = 0;
+    },5000);
 }
+
 
 for(let i = 0; i < anchors.length; i++) {
     anchors[i].addEventListener("click", function(){
-        console.log("event - click on ", anchors[i]);
         showDetails();
         setDetails(anchors[i]);
     })
@@ -21,8 +31,14 @@ for(let i = 0; i < anchors.length; i++) {
 
 function showDetails(){
     mainClass.classList.remove(HIDDEN);
+    detailsContainer.classList.add(IS_POINT);
+
+    setTimeout(function() {
+        detailsContainer.classList.remove(IS_POINT);
+    },1)
 }
 
 function hideDetails(){
     mainClass.classList.add(HIDDEN);
 }
+
