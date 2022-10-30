e036f277e8a5aa03189ec605cf5fba50f8a5d1b4
ArtemOsipchuk <artyom.osipchuk92@gmail.com>
2022-10-30 11:42:57 +0200

    HW #11

---
+++ app.js
@@ -0,0 +1,18 @@
+
+console.log("Hello world");
+
+let number = 23;
+
+function sumDigits(number) {
+    if (number < 0){
+    number = -number;}
+    let rem = 0, sum = 0;
+    do{
+        rem = number % 10;
+        sum = sum + rem;
+        number = Math.floor(number/10);
+    }while(number > 0);
+    return sum;
+}
+
+console.log(sumDigits(number));
